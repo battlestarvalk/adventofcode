70d2477015e230c8decda09cbb25ea491d8b5df1
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2020-12-08 20:23:05 +0000

    cleaned/optimized code

--- 2020/20201208_aoc08.js
+++ 2020/20201208_aoc08.js
@@ -10,46 +10,45 @@ const test_input = ['nop +0',
                     'jmp -4',
                     'acc +6']
 
-let count = 0,
-    i = 0,
-    duplicateFinder = 0,
-    visited = [];
+function breakLoop (inputArray) {
+  let count = 0,
+      i = 0,
+      duplicateFinder = 0,
+      visited = [];
 
-for(let i = 0; i < day8_input.length;) {
-    var instructions = day8_input[i].split(" ");
+  for(let i = 0; i < inputArray.length;) {
+      var instructions = inputArray[i].split(" ");
 
-    if(duplicateFinder === 1) {
-      if(instructions[0] === "acc") {
-        count = count - Number(instructions[1])
-          }
-        break
-      console.log(count)
-    }
-    
-    for(let j = 0; j < (visited.length - 1); j++) {
-        if(i === visited[j]) {
-            duplicateFinder++
-        }
-    }
+      if(duplicateFinder === 1) {
+          break
+  //       console.log(count)
+      }
 
-    if (instructions[0] === "nop") {
-        visited.push(i)
-        i++
-        console.log(count, "nop", duplicateFinder)
-    }
+      if(visited.includes(i)) {
+          duplicateFinder++
+      }
 
-    else if (instructions[0] === "acc") {
-        count = count + Number(instructions[1])
-        visited.push(i)
-        i++
-        console.log(count, "acc", duplicateFinder)
-    }
+      else if (instructions[0] === "nop") {
+          visited.push(i)
+          i++
+  //         console.log(count, "nop", duplicateFinder)
+      }
 
-    else if (instructions[0] === "jmp") {
-        visited.push(i)
-        i += Number(instructions[1])
-        console.log(count, "jmp", duplicateFinder)
-    }
+      else if (instructions[0] === "acc") {
+          count = count + Number(instructions[1])
+          visited.push(i)
+          i++
+  //         console.log(count, "acc", duplicateFinder)
+      }
 
+      else if (instructions[0] === "jmp") {
+          visited.push(i)
+          i += Number(instructions[1])
+  //         console.log(count, "jmp", duplicateFinder)
+      }
+
+  }
+  return count
 }
-count
+
+breakLoop(day8_input)
