5759d66f7d07938763ff4de620f4421cc706501f
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2020-12-08 14:34:00 +0000

    Update 20201208_aoc08.js

--- 2020/20201208_aoc08.js
+++ 2020/20201208_aoc08.js
@@ -10,20 +10,6 @@ const test_input = ['nop +0',
                     'jmp -4',
                     'acc +6']
 
-// function characterAppearance(array, value) {
-//     var count = 0;
-//     array.forEach(function(letter) {
-//     if (letter === value) {
-//         return count++
-//     } 
-//     })
-//     return count;
-// }
-
-// var double = day8_input.filter(function (instruction) {
-//     return characterAppearance(day8_input, instruction) === 2
-// })[0]
-
 let count = 0,
     i = 0,
     duplicateFinder = 0,
@@ -32,38 +18,38 @@ let count = 0,
 for(let i = 0; i < day8_input.length;) {
     var instructions = day8_input[i].split(" ");
 
-    if(duplicateFinder === 2) {
+    if(duplicateFinder === 1) {
+      if(instructions[0] === "acc") {
+        count = count - Number(instructions[1])
+          }
         break
       console.log(count)
     }
     
-//     if(day8_input[i] === double) {
-//         duplicateFinder++
-//     }
-    for(let j = 0; j < (visited.length - 2); j++;) {
+    for(let j = 0; j < (visited.length - 1); j++) {
         if(i === visited[j]) {
             duplicateFinder++
         }
     }
 
     if (instructions[0] === "nop") {
-        i++
         visited.push(i)
-        console.log(count, "nop", i, duplicateFinder)
+        i++
+        console.log(count, "nop", duplicateFinder)
     }
 
     else if (instructions[0] === "acc") {
         count = count + Number(instructions[1])
-        i++
         visited.push(i)
-        console.log(count, "acc", i, Number(instructions[1]), duplicateFinder)
+        i++
+        console.log(count, "acc", duplicateFinder)
     }
 
     else if (instructions[0] === "jmp") {
-        i += Number(instructions[1])
         visited.push(i)
-        console.log(count, "jmp", i, Number(instructions[1]), duplicateFinder)
+        i += Number(instructions[1])
+        console.log(count, "jmp", duplicateFinder)
     }
-    
+
 }
 count
