f0898066b16c614a395a45f079f03742da4c536b
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2020-12-10 13:40:34 +0000

    Update 20201209_aoc09.js

--- 2020/20201209_aoc09.js
+++ 2020/20201209_aoc09.js
@@ -1,23 +1,4 @@
-const test_input = [35,
-                    20,
-                    15,
-                    25,
-                    47,
-                    40,
-                    62,
-                    55,
-                    65,
-                    95,
-                    102,
-                    117,
-                    150,
-                    182,
-                    127,
-                    219,
-                    299,
-                    277,
-                    309,
-                    576]
+const test_input = [35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 50, 182, 127, 219, 299, 277, 309, 576]
 
 const day9_input = document.querySelector('pre').textContent.split('\n').map(Number);
 
@@ -48,27 +29,33 @@ function findWeakness (arr) {
 	}
 return weakness
 }
-findWeakness(day9_input)
+const weakness_figure = findWeakness(day9_input)
 
-const weakness_figure = 373803594;
 function sumTotal (arr) {
     return arr.reduce(function(a, b) {return a+b})
 }
 
-const test_weakness = 127;
-var contiguous_window = [test_input[0]]
-
-for(let i = 0; i < test_input.length; i++) {
-    if(sumTotal(contiguous_window) < test_weakness) {
-        contiguous_window.push(test_input[i+1])
-    }
-    if(sumTotal(contiguous_window) > test_weakness) {
-        contiguous_window.shift()
-    }
-    if(sumTotal(contiguous_window) === test_weakness) {
-        break;
-    }
-
+function findWindow (arr, weakness) {
+	for(let j=2; j < arr.length; j++) {
+		var roll_window = arr.slice(0,j),
+		    found = false;
+		for(let i = j; i < arr.length; i++) {
+				if(sumTotal(roll_window) != weakness) {
+				roll_window.shift()
+				roll_window.push(arr[i])
+				}
+		    else if(sumTotal(roll_window) === weakness) {
+			found = true;
+			return roll_window
+		    }
+		}
+	   if(found === true) {
+		return roll_window
+		break;
+		}
+	    }
 }
+var contiguous_window = findWindow(day9_input, weakness_figure)
+
 contiguous_window.sort()
 contiguous_window[0] + contiguous_window[contiguous_window.length - 1]
