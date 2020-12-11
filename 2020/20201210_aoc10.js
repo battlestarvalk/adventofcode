67432d08abf4e7b06661b21764b51729b0b111cc
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2020-12-11 23:01:20 +0000

    improved readability

--- 2020/20201210_aoc10.js
+++ 2020/20201210_aoc10.js
@@ -1,5 +1,5 @@
 const test_input = [28,33,18,42,31,14,46,20,48,47,24,23,49,45,19,38,39,11,1,32,25,35,8,17,7,9,4,2,34,10,3]
-const test_input_short = [16,10,15,5,1,11,7,19,6,12,4]
+const sTest = [16,10,15,5,1,11,7,19,6,12,4]
 
 const day10_input = document.querySelector('pre').textContent.split('\n').map(Number)
 
@@ -28,31 +28,36 @@ function adapterUsage (arr) {
 
 adapterUsage(day10_input)
 
-var count = 0;
-for(let i = 0; i < test_input_short.length; i++) {
-        if(test_input_short.includes(test_input_short[i] - 1)) {
-        console.log(test_input_short[i], "one below")
-        count++
-        }
-    if(test_input_short.includes(test_input_short[i] + 1)) {
-        console.log(test_input_short[i], "one above")
-        count++
-        }
-    if(test_input_short.includes(test_input_short[i] - 2)) {
-        console.log(test_input_short[i], "two below")
-        count++
-        }
-    if(test_input_short.includes(test_input_short[i] + 1)) {
-        console.log(test_input_short[i], "two above")
-        count++
-        }
+function tribonacci (arr) {
+    var count = 0;
+      for(let i = 0; i < arr.length; i++) {
+              if(arr.includes(arr[i] - 1)) {
+              console.log(arr[i], "one below")
+              count++
+              }
+          if(arr.includes(arr[i] + 1)) {
+              console.log(arr[i], "one above")
+              count++
+              }
+          if(arr.includes(arr[i] - 2)) {
+              console.log(arr[i], "two below")
+              count++
+              }
+          if(arr.includes(arr[i] + 1)) {
+              console.log(arr[i], "two above")
+              count++
+              }
 
-    else if ( ( (test_input_short[i] - test_input_short[i-1]) > 2
-                            || test_input_short[i-1] === undefined )
-                        || ( (test_input_short[i+1] - test_input_short[i]) > 2)
-                            || test_input_short[i+1] === undefined )
-    {
-        console.log(test_input_short[i], "three")
-        count++
-        }
+          else if ( ( (arr[i] - arr[i-1]) > 2
+                                  || arr[i-1] === undefined )
+                              || ( (arr[i+1] - arr[i]) > 2)
+                                  || arr[i+1] === undefined )
+          {
+              console.log(arr[i], "three")
+              count++
+              }
+      }
+    return count
 }
+
+tribonacci(sTest)
