2bcc6dbe0f4fd7654ea464986f4bd231b5fc9812
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2020-12-12 19:50:58 +0000

    finish part 1

--- 2020/20201212_aoc12.js
+++ 2020/20201212_aoc12.js
@@ -1,8 +1,10 @@
 const test_input = ['F10','N3','F7','R90','F11']
+const input = document.querySelector('pre').textContent.split('\n').slice(0, -1);
 
 let compass = 0,
     manhattan = {"NS": 0, "EW": 0};
     
+// change the compass to rotate
 function compassRotation (str) {
   var movementdirection = Number(str.substring(1)) / 90
   if( str.startsWith("R") ) {
@@ -11,21 +13,26 @@ function compassRotation (str) {
   else if ( str.startsWith("L") ) {
       compass = compass -= movementdirection
   }
-  return Math.abs(compass) % 4
+  compass = (compass % 4)
+  return compass
 }
 
+// the compass is stored numerically, so we want to convert it to a letter to match the rest of the input
 function direction (num) {
   switch(num) {
     case 0:
           return "E";
           break;
     case 1:
+    case -3:
           return "S";
           break;
     case 2:
+    case -2:
           return "W";
           break;
     case 3:
+    case -1:
           return "N";
           break;
     default:
@@ -33,41 +40,61 @@ function direction (num) {
   }
 }
 
-compass = 0;
-manhattan = {"NS": 0, "EW": 0};
+// this is the compass movement from the final function, in case you want it separate :)
+// function compassMovement (str) {
+//         switch(str.charAt(0)) {
+//         case "N":
+//             manhattan["NS"] += Number(str.substring(1));
+//             break;
+//         case "S":
+//             manhattan["NS"] -= Number(str.substring(1));
+//             break;
+//         case "E":
+//             manhattan["EW"] += Number(str.substring(1));
+//             break;
+//         case "W":
+//             manhattan["EW"] -= Number(str.substring(1));
+//             break;
+//         default:
+//             console.log("idk!");
+//     }
+// }
 
-function compassMovement (str) {
-        switch(str.charAt(0)) {
-        case "N":
-            manhattan["NS"] += Number(str.substring(1));
-            break;
-        case "S":
-            manhattan["NS"] -= Number(str.substring(1));
-            break;
-        case "E":
-            manhattan["EW"] += Number(str.substring(1));
-            break;
-        case "W":
-            manhattan["EW"] -= Number(str.substring(1));
-            break;
-        default:
-            console.log("idk!");
-    }
+// part 1
+function goTheDistance (arr) {
+
+    arr.forEach( function(movement) {
+//        console.log("start", movement, compass, manhattan)
+        if(movement.startsWith("F")) {
+            movement = direction(compass).concat(movement.substring(1));
+        }
+
+        if(movement.startsWith("R") || movement.startsWith("L")) {
+            compassRotation(movement)
+        }
+
+        else {
+		console.log("movement", movement, compass, movement.substring(1))
+          switch(movement.charAt(0)) {
+            case "N":
+                manhattan["NS"] += Number(movement.substring(1));
+                break;
+            case "S":
+                manhattan["NS"] -= Number(movement.substring(1));
+                break;
+            case "E":
+                manhattan["EW"] += Number(movement.substring(1));
+                break;
+            case "W":
+                manhattan["EW"] -= Number(movement.substring(1));
+                break;
+            default:
+                console.log("idk!");
+        }
+      }
+// 	console.log("end", movement, compass, manhattan)
+    })
+    
+    return Math.abs(manhattan["NS"])+Math.abs(manhattan["EW"])
 }
 
-test_input.forEach( function(movement) {
-    if(movement.startsWith("F")) {
-        movement = direction(compass).concat(test_input[2].substring(1))
-    }
-
-    if(movement.startsWith("R") || movement.startsWith("L")) {
-        compassRotation(movement)
-    }
-
-    else {
-            compassMovement(movement)
-  }
-
-})
-
-manhattan
