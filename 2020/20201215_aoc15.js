2a40b93090fb71b74dfc414b82a79bbebd4deffc
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2020-12-15 22:24:24 +0000

    Update 20201215_aoc15.js

--- 2020/20201215_aoc15.js
+++ 2020/20201215_aoc15.js
@@ -1,3 +1,19 @@
 const test_input = [0,3,6]
 const input = [0,14,1,3,7,9]
 
+obj = {};
+for(let i = 0; i < 10; i++) {
+            if(obj.hasOwnProperty(tmp_input[i])) {
+                obj[tmp_input[i]].push(i)
+        obj_length = obj[tmp_input[i]].length
+                if (obj_length > 1) {
+                tmp_input.push( obj[tmp_input[i]][obj_length - 2] - obj[tmp_input[i]][obj_length - 1] ) 
+                }
+                else {
+                        tmp_input.push( 0 )
+                }
+      }
+            else {
+                obj[tmp_input[i]] = [i]
+            }
+}
