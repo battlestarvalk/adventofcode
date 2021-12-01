5ce6748f76d61b1efec9065c62e07bdf8a783f0e
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2021-12-01 13:52:21 +0000

    Update and rename aoc2021_day1.js to aoc2021_day1_sonarSweep.js

--- 2021/aoc2021_day1.js
+++
@@ -1,9 +0,0 @@
-/* this answer is 2 too small */
-var input = document.querySelector('pre').textContent.split('\n').slice(0,-1),
-    output = [];
-for(var i=0; i<input.length; i++) {
-    if(input[i+1] > input[i]) {
-        output.push(input[i+1])
-    }
-}
-output.length
---
+++ 2021/aoc2021_day1_sonarSweep.js
@@ -0,0 +1,32 @@
+/*part one*/
+var input = document.querySelector('pre').textContent.split('\n').slice(0,-1),
+    output = [];
+for(var i=0; i<input.length; i++) {
+    if(input[i]-input[i-1] > 0) {
+        output.push(input[i])
+    }
+}
+output.length
+
+/*part two*/
+
+function rollWindow (array) {
+var first_run = array.map(Number),
+    temp_array = [],
+    output = [];
+
+for(var i=0; i<first_run.length; i++) {
+    temp_array.push([first_run[i-2], first_run[i-1], first_run[i]].reduce(function(acc, val) { return acc + val; }, 0))
+}
+temp_array.slice(2)
+
+for(var i=0; i<temp_array.length; i++) {
+    if(temp_array[i]-temp_array[i-1] > 0) {
+        output.push(temp_array[i])
+    }
+}
+
+return output.length
+
+}
+rollWindow(input)
