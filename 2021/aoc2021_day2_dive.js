5b5f8f49826a7b1cd0014ae41be5bcbfe436b7f2
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2021-12-02 09:19:49 +0000

    Create aoc2021_day2_dive.js

---
+++ 2021/aoc2021_day2_dive.js
@@ -0,0 +1,66 @@
+/* setup */
+var test_input = ['forward 5', 'down 5', 'forward 8', 'up 3', 'down 8', 'forward 2']
+
+var input = document.querySelector('pre').textContent.split('\n').slice(0,-1)
+
+/*part one*/
+
+function travelDistance(array) {
+    var depth = 0,
+        distance = 0;
+
+    for(var i = 0; i<array.length;i++) {
+        var direction = array[i].split(" ")[0],
+            increments = Number(array[i].split(" ")[1]);
+
+        if(direction == "forward") {
+            distance = distance +increments
+        }
+
+        if(direction == "up") {
+            depth = depth -increments
+        }
+
+        if(direction == "down") {
+            depth = depth +increments
+        }
+
+    }
+
+return depth*distance
+
+}
+
+travelDistance(input)
+
+/*part two*/
+
+function travelAim(array) {
+    var depth = 0,
+        distance = 0,
+        aim = 0;
+
+    for(var i = 0; i<array.length;i++) {
+        var direction = array[i].split(" ")[0],
+            increments = Number(array[i].split(" ")[1]);
+
+        if(direction == "forward") {
+            distance = distance +increments
+            depth = depth +(increments*aim)
+        }
+
+        if(direction == "up") {
+            aim = aim -increments
+        }
+
+        if(direction == "down") {
+            aim = aim +increments
+        }
+
+    }
+
+return depth*distance
+
+}
+
+travelAim(input)
