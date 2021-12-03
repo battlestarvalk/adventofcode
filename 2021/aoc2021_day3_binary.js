bb0564077fb18e2e88d2e015138dea339e8cd8cb
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2021-12-03 11:41:39 +0000

    Update aoc2021_day3_binary.js

--- 2021/aoc2021_day3_binary.js
+++ 2021/aoc2021_day3_binary.js
@@ -39,38 +39,105 @@ return parseInt(Number(tmp_array_gamma.join("")), 2)*parseInt(Number(tmp_array_e
 Converter(input) //1071734
 
 /*part two*/
-var test = input,
-    oxy_gen = 0;
+var oxy_input = input,
+    car_input = input,
+    oxy_gen = 0,
+    car_gen = 0;
 
 
-for(var j=0; j<(test[0].length);j++) {
-    if(test.length == 1) {
-        oxy_gen = parseInt(Number(test[0]), 2)
+for(var j=0; j<oxy_input[0].length;j++) {
+
+    if(oxy_input.length == 1) {
         break;
     }
+
+    /*if(car_test.length == 1) {
+        car_gen = parseInt(Number(test[0]), 2);
+    }*/
 
 var tmp_zeros_array = [],
     tmp_ones_array = [],
     zeros = 0,
     ones = 0;
 
-    for(var i = 0; i<test.length;i++) {
-        if(test[i].charAt(j) == '0') {
+    for(var i = 0;i<oxy_input.length;i++) {
+        if(oxy_input[i].charAt(j) == '0') {
             zeros++,
             tmp_zeros_array.push(i);
         }
 
-        if(test[i].charAt(j) == '1') {
+        if(oxy_input[i].charAt(j) == '1') {
             ones++,
             tmp_ones_array.push(i);
         }
     }
+
     if(zeros > ones) {
-        test = test.filter(function(value, index) {return tmp_ones_array.indexOf(index) == -1});
+        oxy_input = oxy_input.filter(function(value, index) {return tmp_ones_array.indexOf(index) == -1})
+        //car_test = test.filter(function(value, index) {return tmp_zeros_array.indexOf(index) == -1});
         
     }
 
-    else if(ones > zeros) {
-        test = test.filter(function(value, index) {return tmp_zeros_array.indexOf(index) == -1});
+    if(ones > zeros) {
+        oxy_input = oxy_input.filter(function(value, index) {return tmp_zeros_array.indexOf(index) == -1})
+        //car_test = test.filter(function(value, index) {return tmp_ones_array.indexOf(index) == -1});
     }
+
+    if(ones == zeros) {
+        oxy_input = oxy_input.filter(function(value, index) {return tmp_zeros_array.indexOf(index) == -1})
+        //car_test = test.filter(function(value, index) {return tmp_ones_array.indexOf(index) == -1});
+    }
+
 }
+
+oxy_gen = parseInt(Number(oxy_input[0]), 2)
+
+for(var j=0; j<oxy_input[0].length;j++) {
+
+    if(car_input.length == 1) {
+        break;
+    }
+
+    /*if(car_test.length == 1) {
+        car_gen = parseInt(Number(test[0]), 2);
+    }*/
+
+var tmp_zeros_array = [],
+    tmp_ones_array = [],
+    zeros = 0,
+    ones = 0;
+
+    for(var i = 0;i<car_input.length;i++) {
+        if(car_input[i].charAt(j) == '0') {
+            zeros++,
+            tmp_zeros_array.push(i);
+        }
+
+        if(car_input[i].charAt(j) == '1') {
+            ones++,
+            tmp_ones_array.push(i);
+        }
+    }
+
+    if(zeros > ones) {
+        //oxy_input = oxy_input.filter(function(value, index) {return tmp_ones_array.indexOf(index) == -1})
+        car_input = car_input.filter(function(value, index) {return tmp_zeros_array.indexOf(index) == -1});
+        
+    }
+
+    if(ones > zeros) {
+        //oxy_input = oxy_input.filter(function(value, index) {return tmp_zeros_array.indexOf(index) == -1})
+        car_input = car_input.filter(function(value, index) {return tmp_ones_array.indexOf(index) == -1});
+    }
+
+    if(ones == zeros) {
+        //oxy_input = oxy_input.filter(function(value, index) {return tmp_zeros_array.indexOf(index) == -1})
+        car_input = car_input.filter(function(value, index) {return tmp_ones_array.indexOf(index) == -1});
+    }
+
+}
+
+car_gen = parseInt(Number(car_input[0]), 2)
+
+oxy_gen*car_gen
+//6124992
