6b9bf0e0970438b21a7c6968e4bb5abf49a1725d
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2021-12-04 12:23:04 +0000

    Update aoc2021_day4_giantSquid.js

--- 2021/aoc2021_day4_giantSquid.js
+++ 2021/aoc2021_day4_giantSquid.js
@@ -1,5 +1,5 @@
 var input = document.querySelector('pre').textContent.split('\n').slice(0,-1),
-    called_nos = input[0],
+    called_nos = input[0].split(",").map(Number),
     boards = input.slice(2);
 
 numerical_board = []
@@ -15,14 +15,14 @@ for(var j = 0;j<(numerical_board.length/25); j++) {
 }
 
 for(var k=0;k<mini_boards.length;k++) {
-	for(var l = 0;l<called_nos.length;l++) {
-		var idx = mini_boards[k].indexOf(called_nos[l], l)
-			if(idx == -1) {
-				break;
-			}
-			else {
-				console.log(idx);
-				idx = mini_boards[k].indexOf(called_nos[l], l)
-			}
-	}
+    for(var l = 0;l<called_nos.length;l++) {
+        var idx = mini_boards[k].indexOf(called_nos[l], l)
+            if(idx == -1) {
+                break;
+            }
+            else {
+                console.log(idx);
+                idx = mini_boards[k].indexOf(called_nos[l], l)
+            }
+    }
 }
