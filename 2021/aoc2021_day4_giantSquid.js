1b05a1f52956dff2406f6105d8bbe39a9c2f6742
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2021-12-05 11:30:32 +0000

    Update aoc2021_day4_giantSquid.js

--- 2021/aoc2021_day4_giantSquid.js
+++ 2021/aoc2021_day4_giantSquid.js
@@ -9,6 +9,7 @@ for(var i = 0; i<boards.length;i++) {
 
 numerical_board = numerical_board.map(Number)
 
+//each array part will represent one board
 mini_boards = [];
 for(var j = 0;j<(numerical_board.length/25); j++) {
     mini_boards.push(numerical_board.slice((25*j), (25*(j+1))))
@@ -17,45 +18,60 @@ for(var j = 0;j<(numerical_board.length/25); j++) {
 /*prep for both parts*/
 
 function bingoCaller(indexCall) {
-    
+    // % defines the column, trun defines the row. adds one to each array counter
     return bingocol[indexCall%5]++,
-                 bingorow[Math.trunc(indexCall/5)]++;
+           bingorow[Math.trunc(indexCall/5)]++;
 
 }
 
 filled_boxes = [],
 winning_round = [],
 winning_num = [];
+
+//first for loop - each board
 for(var k = 0; k<mini_boards.length; k++) {
+// need to know all called numbers
   filled_boxes.push([])
+// new row/col counter
     var bingorow = [0,0,0,0,0],
         bingocol = [0,0,0,0,0];
+
+//second for loop - each called number
     for(var m = 0;m<called_nos.length;m++) {
+    // tick off every instance of a number
     var idx = mini_boards[k].indexOf(called_nos[m], idx + 1)
 
     while (idx != -1) {
-      //console.log(k, called_nos[m], idx, bingocol, bingorow)
-            bingoCaller(idx)
+//use the bingoCaller function from earlier to fill the row/columns ticked off
+      bingoCaller(idx)
       filled_boxes[k].push(idx)
       idx = mini_boards[k].indexOf(called_nos[m], idx + 1);
     }
+// after each called number, check if any row/number has 5 yet    
         if(Math.max(...bingorow) == 5 || Math.max(...bingocol) == 5) {
             winning_round.push(m)
             winning_num.push(called_nos[m])
+// if so, stop checking numbers
             break;
         }
   }
 }
 
 /*part one*/
-
+//winning round now holds the number of which number was called and when it was called as a board wins.
+//win_board reports back which board number won
 win_board = winning_round.indexOf(Math.min(...winning_round))
+//win_num reports back which number was the winner
 win_num = winning_num[winning_round.indexOf(Math.min(...winning_round))]
 
+//sum rem filters the winning board by all uncalled numbers and sums those remaining
 sumRem = mini_boards[win_board].filter(function(value, index) {return filled_boxes[win_board].indexOf(index) == -1}).reduce((x,y) => x+y)
+
+//final result = sum of remaining muultiplied by winning number
 sumRem*win_num
 
 /*part two*/
+//as above in reverse
 loss_board = winning_round.indexOf(Math.max(...winning_round))
 loss_num = winning_num[winning_round.indexOf(Math.max(...winning_round))]
 
