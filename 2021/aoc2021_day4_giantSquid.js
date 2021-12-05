a09dc0bdec9f63751e23104c8f17c4a9899bd999
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2021-12-05 11:11:51 +0000

    Update aoc2021_day4_giantSquid.js

--- 2021/aoc2021_day4_giantSquid.js
+++ 2021/aoc2021_day4_giantSquid.js
@@ -15,16 +15,36 @@ for(var j = 0;j<(numerical_board.length/25); j++) {
 }
 
 /*part one*/
-filled_boxes = []
 
+function bingoCaller(indexCall) {
+    
+    return bingocol[indexCall%5]++,
+                 bingorow[Math.trunc(indexCall/5)]++;
+
+}
+
+filled_boxes = [],
+winning_round = [];
 for(var k = 0; k<mini_boards.length; k++) {
   filled_boxes.push([])
-	for(var m = 0;m<called_nos.length;m++) {
+    var bingorow = [0,0,0,0,0],
+        bingocol = [0,0,0,0,0];
+    for(var m = 0;m<called_nos.length;m++) {
     var idx = mini_boards[k].indexOf(called_nos[m], idx + 1)
+
     while (idx != -1) {
-      //console.log(k, called_nos[k], idx)
+      //console.log(k, called_nos[m], idx, bingocol, bingorow)
+            bingoCaller(idx)
       filled_boxes[k].push(idx)
       idx = mini_boards[k].indexOf(called_nos[m], idx + 1);
     }
+        if(Math.max(...bingorow) == 5 || Math.max(...bingocol) == 5) {
+            winning_round.push(m)
+            break;
+        }
   }
 }
+
+win_board = winning_round.indexOf(Math.min(...winning_round))
+mini_boards[win_board].filter(function(value, index) {return filled_boxes[win_board].indexOf(index) == -1})
+mini_boards[win_board].filter(function(value, index) {return filled_boxes[win_board].indexOf(index) == -1}).reduce((x,y) => x+y)
