5376d00ff2fb12374452a1061c3a6a88056b966d
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2021-12-06 19:59:35 +0000

    Update aoc2021_day6_lanternfish.js

--- 2021/aoc2021_day6_lanternfish.js
+++ 2021/aoc2021_day6_lanternfish.js
@@ -1,12 +1,13 @@
-/*test = [3,4,3,1,2]
-
+/*
+test = [3,4,3,1,2]
 for(var i=0;i<test.length;i++) {
+  if(i > 18 ) {
+    break;
+  }
+    var regenerations = Math.round((19-test[i])/6)
 
-    regenerations = Math.round((19-test[i])/6)
-    //test.push(test[i])
-
-    //console.log(i, test[i], regenerations, test)
-
+    console.log(i, test[i], regenerations, test)
+if(regenerations > 0) {
     for(var j = 1; j<regenerations+1;j++) {
         //console.log(i, j, ((test[i])+(7*j)))
         if((test[i])+(7*j) > 19) {
@@ -16,12 +17,11 @@ for(var i=0;i<test.length;i++) {
             test.push((test[i])+(7*j)) 
         }
     }
-
-  if(test.length > 70) {
-    break;
-  }
 }
 
+    test.push(test[i]+9)
+
+}
 test.filter(x => x < 19).length
 */
 
