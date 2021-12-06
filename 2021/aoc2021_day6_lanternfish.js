f5df78a3602d1f270a219a9b8586471aec0633db
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2021-12-06 11:54:31 +0000

    Update aoc2021_day6_lanternfish.js

--- 2021/aoc2021_day6_lanternfish.js
+++ 2021/aoc2021_day6_lanternfish.js
@@ -6,3 +6,21 @@ for(var i=0;i<test.length;i++) {
 }
 
 test_array.reduce((a,b) => (a+b))
+
+test = [3,4,3,1,2]
+spawndays = new Array (19).fill(0)
+for(var i=0;i<test.length;i++) {
+    spawndays[6-test[i]]++
+    regenerations = Math.round((18-test[i])/6)
+    test.push((6-test[i])+2)
+    console.log(i, regenerations)
+    for(var j = 0; j<regenerations;j++) {
+        console.log(i, j, (6-test[i])+(7*j))
+        spawndays[(6-test[i])+(7*j)]++
+    }
+
+  if((18-test[i]) < 0 || i == 20) {
+    break;
+  }
+    
+}
