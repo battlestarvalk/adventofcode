4d8919d26a1d2b31e6b3a96e68d731d14e798297
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2021-12-08 09:34:57 +0000

    Update aoc2021_day8_sevenSegment.js

--- 2021/aoc2021_day8_sevenSegment.js
+++ 2021/aoc2021_day8_sevenSegment.js
@@ -33,3 +33,59 @@ for(var i=0; i<number.length; i++) {
     }
 }
 target_num
+
+/*part two*/
+converted_number = []
+for(var j=0; j<number.length;j++) {
+    converted_number.push([])
+    for(var i=0; i<number[j].length;i++) {
+
+        if(target.includes(number[j][i].length)) {
+            switch(number[j][i].length) {
+                case 7:
+                    converted_number[j].push(8);
+                    break;
+                case 4:
+                    converted_number[j].push(4);
+                    break;
+                case 3:
+                    converted_number[j].push(7);
+                    break;
+                case 2:
+                    converted_number[j].push(1);
+                    break;
+        }
+        }
+
+        if(number[j][i].length == 5) {
+
+            if(/g/.test(number[j][i])) {
+                converted_number[j].push(2)
+            }
+
+            if(/a/.test(number[j][i])) {
+                converted_number[j].push(3)
+            }
+
+            if(/e/.test(number[j][i])) {
+                converted_number[j].push(5)
+            }
+        }
+
+        if(number[j][i].length == 6) {
+
+            if(/f/.test(number[j][i])) {
+                if(/g/.test(number[j][i])) {
+                    converted_number[j].push(6)
+                }
+
+                if(/a/.test(number[j][i])) {
+                    converted_number[j].push(9)
+                }
+            }
+
+            else {converted_number[j].push(0)}
+        }
+    }
+}
+converted_number
