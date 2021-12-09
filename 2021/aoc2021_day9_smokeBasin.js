94fdc0d2a83488341fe485426c32e4979138151e
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2021-12-09 09:08:18 +0000

    Update aoc2021_day9_smokeBasin.js

--- 2021/aoc2021_day9_smokeBasin.js
+++ 2021/aoc2021_day9_smokeBasin.js
@@ -5,24 +5,24 @@ lowpoints = []
 for(var i=0; i<input.length;i++) {
     for(var j=0; j<input[i].length; j++) {
         if(input[i][j] == 0) {
-            lowpoints.push(input[i][j])
+            lowpoints.push(1)
         }
         else {
             compare = [input[i][j]]
             //leftmost index
-            if(input[i][j-1] != undefined) {compare.push(input[i][j-1])}
+            if(input[i][j-1] != undefined && input[i][j-1] <= input[i][j]) {compare.push(input[i][j-1])}
             //rightmost index
-            if(input[i][j+1] != undefined) {compare.push(input[i][j+1])}
+            if(input[i][j+1] != undefined && input[i][j+1] <= input[i][j]) {compare.push(input[i][j+1])}
             //one below
-            if(input[i+1] != undefined && input[i+1][j] != undefined) {compare.push(input[i+1][j])}
+            if(input[i+1] != undefined && input[i+1][j] != undefined && input[i+1][j] <= input[i][j]) {compare.push(input[i+1][j])}
             //one above
-            if(input[i-1] != undefined && input[i-1][j] != undefined) {compare.push(input[i-1][j])}
+            if(input[i-1] != undefined && input[i-1][j] != undefined && input[i-1][j] <= input[i][j]) {compare.push(input[i-1][j])}
 
-            if(Math.min(...compare) == input[i][j]) {
-                lowpoints.push(input[i][j])
+            if(compare.length == 1) {
+                lowpoints.push(input[i][j] + 1)
             }
         }
     }
 }
 
-lowpoints.map(x => x+1).reduce((a,b) => (a+b))
+lowpoints.reduce((a,b) => (a+b))
