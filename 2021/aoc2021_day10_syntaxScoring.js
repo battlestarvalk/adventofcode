5d7f54a9012cdac2e4e46a29e71991a6de211309
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2021-12-10 11:29:28 +0000

    Update aoc2021_day10_syntaxScoring.js

--- 2021/aoc2021_day10_syntaxScoring.js
+++ 2021/aoc2021_day10_syntaxScoring.js
@@ -1,4 +1,7 @@
-input = document.querySelector("body > main > article > pre:nth-child(12) > code").innerText.split("\n").join('').split('')
+input_full = document.querySelector("body > main > article > pre:nth-child(12) > code").innerText.split("\n").join('').split('')
+input = document.querySelector("body > main > article > pre:nth-child(12) > code").innerText.split("\n")
+
+line2_test = input[2].split('')
 
 pairs = {
 "(":")",
@@ -11,7 +14,7 @@ expected_string = []
 
 for(var i=0;i<line2_test.length;i++) {
     //console.log(i, "start", expected_string)
-    if(pairs[input[i]]) {
+    if(pairs[line2_test[i]]) {
         expected_string.push(pairs[line2_test[i]])
     }
     else if(expected_string.indexOf(line2_test[i]) == -1) {
@@ -20,6 +23,7 @@ for(var i=0;i<line2_test.length;i++) {
         }
     else {
         expected_string.splice(expected_string.indexOf(line2_test[i]), 1)
+        //console.log("mismatch", line2_test[i])
     }
     //console.log(i, "end", expected_string)
 }
