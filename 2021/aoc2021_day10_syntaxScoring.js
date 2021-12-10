3237b9fa0b1d7024c4d82ce71cf4a14bd12461de
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2021-12-10 10:27:27 +0000

    Create aoc2021_day10_syntaxScoring.js

---
+++ 2021/aoc2021_day10_syntaxScoring.js
@@ -0,0 +1,27 @@
+input = document.querySelector("body > main > article > pre:nth-child(12) > code").innerText.split("\n").join('').split('')
+
+pairs = {
+"(":")",
+"[":"]",
+"{":"}",
+"<":">"
+}
+
+expected_string = []
+
+for(var i=0;i<line2_test.length;i++) {
+    //console.log(i, "start", expected_string)
+    if(pairs[input[i]]) {
+        expected_string.push(pairs[line2_test[i]])
+    }
+    else if(expected_string.indexOf(line2_test[i]) == -1) {
+        console.log("mismatch", line2_test[i]); 
+        break;
+        }
+    else {
+        expected_string.splice(expected_string.indexOf(line2_test[i]), 1)
+    }
+    //console.log(i, "end", expected_string)
+}
+
+expected_string
