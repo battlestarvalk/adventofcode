ed95ebfbb8d3a23cb8a4c9e747b3fd0f12e500d6
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2021-12-11 14:46:59 +0000

    Update aoc2021_day11_dumboOctopus.js

--- 2021/aoc2021_day11_dumboOctopus.js
+++ 2021/aoc2021_day11_dumboOctopus.js
@@ -1,10 +1,12 @@
 input = document.querySelector('pre').textContent.split('\n').map(x => x.split('')).map((x,y) => x.map(y => Number(y))).slice(0,-1)
 
+/*setup*/
+
 function flashCheck (octoGrid, flashGrid) {
 
   for(var i=0; i<octoGrid.length; i++) {
       for(var j=0; j<octoGrid[i].length; j++) {
-          if(octoGrid[i][j] >= 9) {
+          if(octoGrid[i][j] > 9) {
               flashGrid.push([j,i])
               octoGrid[i].splice(j, 1, 0)
           }
@@ -17,62 +19,82 @@ function adjacentFlashes (flashArr, stepArr) {
   for(var k=0;k<flashArr.length;k++) {
       col = flashArr[k][0]
       row = flashArr[k][1]
-      arrayLen = stepArr.length-1
+      arrayLen = (stepArr.length)-1
 
       //all row above steps
       if(stepArr[row-1]) {
           //top left diagonal
-          if(col != 0 && stepArr[row-1][col-1] != 0) {stepArr[row-1][col-1] = stepArr[row-1][col-1] +1}
+          if(col != 0 && stepArr[row-1][col-1] != 0) {stepArr[row-1][col-1] +=1}
           //directly above
-          if(stepArr[row-1][col] != 0) {stepArr[row-1][col] = stepArr[row-1][col] +1}
+          if(stepArr[row-1][col] != 0) {stepArr[row-1][col] +=1}
           //top right diagonal
-          if(col != arrayLen && stepArr[row-1][col+1] != 0) {stepArr[row-1][col+1] = stepArr[row-1][col+1] +1}
+          if(col != arrayLen && stepArr[row-1][col+1] != 0) {stepArr[row-1][col+1] +=1}
       }
 
       //all row below steps
       if(stepArr[row+1]) {
           //bottom left diagonal
-          if(col != 0 && stepArr[row+1][col-1] != 0) {stepArr[row+1][col-1] = stepArr[row+1][col-1] +1}
+          if(col != 0 && stepArr[row+1][col-1] != 0) {stepArr[row+1][col-1] +=1}
           //directly below
-          if(stepArr[row+1][col] != 0) {stepArr[row+1][col] = stepArr[row+1][col] +1}
+          if(stepArr[row+1][col] != 0) {stepArr[row+1][col] +=1}
           //bottom right diagonal
-          if(col != arrayLen && stepArr[row+1][col+1] != 0) {stepArr[row+1][col+1] = stepArr[row+1][col+1] +1}
+          if(col != arrayLen && stepArr[row+1][col+1] != 0) {stepArr[row+1][col+1] +=1}
       }
 
       //right
-      if(col != arrayLen && stepArr[row][col+1] != 0) {stepArr[row][col+1] = stepArr[row][col+1] +1}
+      if(col != arrayLen && stepArr[row][col+1] != 0) {stepArr[row][col+1] +=1}
       //left
-      if(col != 0 && stepArr[row][col-1] != 0) {stepArr[row][col-1] = stepArr[row][col-1] +1}
+      if(col != 0 && stepArr[row][col-1] != 0) {stepArr[row][col-1] +=1}
   }
 
 }
 
+/*part one*/
 function octoFlashes (inputArr, steps) {
   
   octoGrid = inputArr
   flashes = 0
-  flashed = []
 
-  for(var m=0; m<steps+1;m++) {
+  for(var m=0; m<steps;m++) {
       octoGrid = octoGrid.map((x,y) => x.map(y => y+1))
 
-      for(var f = 0; f<9; f++) {
-				flashed = []
+      for(var f = 0; ;f++) {
+    flashed = []
         flashCheck(octoGrid, flashed)
-				if(flashed.length == 0) {
-					break;
-				}
-        flashes = flashes +(flashed.length)
+        flashes +=(flashed.length)
         adjacentFlashes(flashed, octoGrid)
-        flashed = []
-        flashCheck(octoGrid, flashed)
-				if(flashed.length == 0) {
-					break;
-				}
-        flashes = flashes +(flashed.length)
+        if(flashed.length == 0) {break;}
     }
   }
   return flashes
 }
 
 octoFlashes(input, 100)
+
+/*part two*/
+
+function octoSync (inputArr) {
+  octoGrid = inputArr
+    flashes = 0
+  goal = octoGrid.flat().length
+    syncStep = 0
+
+  for(var m=0;;m++) {
+    if(flashes == goal) {syncStep = m; break;}
+    flashes = 0
+    octoGrid = octoGrid.map((x,y) => x.map(y => y+1))
+
+    for(var f = 0;;f++) {
+      flashed = []
+      flashCheck(octoGrid, flashed)
+      flashes +=(flashed.length)
+      adjacentFlashes(flashed, octoGrid)
+      if(flashed.length == 0) {break;}
+
+    }
+  }
+
+return syncStep
+}
+
+octoSync(input)
