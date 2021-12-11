44aafeb28be08b67acce5c96f92fc01bf4628a3b
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2021-12-11 15:45:30 +0000

    Update and rename aoc2021_day6_lanternfish.js to aoc2021_day06_lanternfish.js

--- 2021/aoc2021_day06_lanternfish.js
+++ 2021/aoc2021_day06_lanternfish.js
@@ -1,3 +1,46 @@
+/*part one*/
+const input = document.querySelector('pre').textContent.split('\n')[0].split(',').map(Number)
+
+function lanternfish(array, days) {
+
+    for(var i=0;i<days;i++) {
+        for(var k=0;k<array.length;k++) {
+            if(array[k] == 0) {
+            array.splice(k, 1, 6)
+            array.push(9)
+            }
+            else {array[k]--}
+        }
+    }
+    return array.length
+
+}
+
+lanternfish(input, 80)
+
+
+/*part two*/
+const input = document.querySelector('pre').textContent.split('\n')[0].split(',').map(Number)
+
+function lanternfish(array, days) {
+
+lifecycles = new Array(9).fill(0)
+array.forEach(x => lifecycles[x]++)
+
+    for(var i=0; i<days;i++) {
+        newfish = lifecycles[0]
+        lifecycles.shift()
+        lifecycles[6] +=newfish
+        lifecycles.push(newfish)
+    }
+
+return lifecycles.reduce((a,b) => (a+b))
+
+}
+
+lanternfish(input, 256)
+
+
 /*
 test = [3,4,3,1,2]
 for(var i=0;i<test.length;i++) {
@@ -24,44 +67,3 @@ if(regenerations > 0) {
 }
 test.filter(x => x < 19).length
 */
-
-/*part one*/
-const input = document.querySelector('pre').textContent.split('\n')[0].split(',').map(Number)
-
-function lanternfish(array, days) {
-
-    for(var i=0;i<days;i++) {
-        for(var k=0;k<array.length;k++) {
-            if(array[k] == 0) {
-            array.splice(k, 1, 6)
-            array.push(9)
-            }
-            else {array[k]--}
-        }
-    }
-    return array.length
-
-}
-
-lanternfish(input, 80)
-
-/*part two*/
-const input = document.querySelector('pre').textContent.split('\n')[0].split(',').map(Number)
-
-function lanternfish(array, days) {
-
-lifecycles = new Array(9).fill(0)
-array.forEach(x => lifecycles[x]++)
-
-    for(var i=0; i<days;i++) {
-        newfish = lifecycles[0]
-        lifecycles.shift()
-        lifecycles[6] = lifecycles[6] +newfish
-        lifecycles.push(newfish)
-    }
-
-return lifecycles.reduce((a,b) => (a+b))
-
-}
-
-lanternfish(input, 256)
