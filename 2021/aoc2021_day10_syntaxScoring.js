d8495034969142227e19b9e701a241e9f8db36d3
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2021-12-11 19:52:47 +0000

    Update aoc2021_day10_syntaxScoring.js

--- 2021/aoc2021_day10_syntaxScoring.js
+++ 2021/aoc2021_day10_syntaxScoring.js
@@ -1,3 +1,4 @@
+/*NOT WORKING*/
 input_full = document.querySelector("body > main > article > pre:nth-child(12) > code").innerText.split("\n").join('').split('')
 input = document.querySelector("body > main > article > pre:nth-child(12) > code").innerText.split("\n")
 
@@ -10,22 +11,19 @@ pairs = {
 "<":">"
 }
 
-expected_string = []
+function groupings (array) {
+    expected_string = []
 
-for(var i=0;i<line2_test.length;i++) {
-    //console.log(i, "start", expected_string)
-    if(pairs[line2_test[i]]) {
-        expected_string.push(pairs[line2_test[i]])
-    }
-    else if(expected_string.indexOf(line2_test[i]) == -1) {
-        console.log("mismatch", line2_test[i]); 
-        break;
+    for(var i=0;i<array.length;i++) {
+        //console.log(i, "start", array[i], expected_string)
+        if(pairs[array[i]]) {
+            expected_string.push(pairs[array[i]])
         }
-    else {
-        expected_string.splice(expected_string.indexOf(line2_test[i]), 1)
-        //console.log("mismatch", line2_test[i])
+        else if(expected_string.indexOf(array[i]) == -1) {
+            console.log("mismatch", array[i]);
+            }
+        else {
+            expected_string.splice(expected_string.indexOf(array[i]), 1)
+        }
     }
-    //console.log(i, "end", expected_string)
 }
-
-expected_string
