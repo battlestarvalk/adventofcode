4a276342322c8daa7e96646ccf1e25265954689e
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2021-12-13 09:40:44 +0000

    Update aoc2021_day13_transparentOrigami.js

--- 2021/aoc2021_day13_transparentOrigami.js
+++ 2021/aoc2021_day13_transparentOrigami.js
@@ -22,34 +22,35 @@ paper_height = Math.max(...height)+1
 // calculation
 
 for(var k=0;k<instructions.length;k++) {
+    //find the line to fold on
     foldline = Number(instructions[k].split('=')[1])
     
+    //if y fold, fold along paper HEIGHT (plus one due to 0-index) and set x as one (where column data is stored in dots)
     if(/y=/.test(instructions[k])) {
-
-        folded_paper = new Array(foldline+1).fill('.').map(x => x = new Array(paper_width).fill('.'))
-
-        for(var i=0; i<dots.length;i++) {
-            if(dots[i][1] >= foldline) {
-            dots[i][1] = (foldline - (dots[i][1]-foldline))
-            }
-            folded_paper[dots[i][1]][dots[i][0]] = '#'
-        }
-
+        paper_height = foldline+1
+        x = 1
     }
-
+    
+    //if x fold, fold along paper WIDTH (plus one due to 0-index) and set x as 0 (where row data is stored in dots)
     if(/x=/.test(instructions[k])) {
-
-        folded_paper = new Array(paper_height).fill('.').map(x => x = new Array(foldline+1).fill('.'))
-
-        for(var i=0; i<dots.length;i++) {
-            if(dots[i][0] >= foldline) {
-                dots[i][0] = (foldline - (dots[i][0]-foldline))
-            }
-
-            folded_paper[dots[i][1]][dots[i][0]] = '#'
-        }
-
+       paper_width = foldline+1
+       x = 0
     }
+    
+    //don't need to build the folded paper each time but maybe I want to :)
+    folded_paper = new Array(paper_height).fill('.').map(x => x = new Array(paper_width).fill('.'))
+    
+    //for each dots coordinate, adjust either row/column (x value) if it's over the fold.
+    for(var i=0; i<dots.length;i++) {
+        if(dots[i][x] >= foldline) {
+            dots[i][x] = (foldline - (dots[i][x]-foldline))
+        }
+        
+        //place all the #/dots on the "paper"
+        folded_paper[dots[i][1]][dots[i][0]] = '#'
+        
+    }
+        
 }
 /* part two - just read across from console */
 folded_paper
