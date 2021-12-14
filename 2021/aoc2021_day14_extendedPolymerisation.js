73fbf4745942b234bedde54ec9da63b4f9a9919a
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2021-12-14 09:46:16 +0000

    Create aoc2021_day14_extendedPolymerisation.js

---
+++ 2021/aoc2021_day14_extendedPolymerisation.js
@@ -0,0 +1,33 @@
+input = document.querySelector('pre').textContent.split('\n').slice(0,-1)
+
+start_string = input[0]
+
+rules = {}
+tmp_input = input.slice(2)
+
+for(var i=0;i<tmp_input.length;i++) {
+    tmp = tmp_input[i].split(' -> ')
+    rules[tmp[0]] = tmp[1]
+}
+
+//breaks when steps is 2
+steps = 1
+tmp_array = []
+start_array = start_string.split('')
+
+for(var m=0;m<steps;m++) {
+    for(var k=0;k<start_array.length;k++) {
+        key_string = start_array[k]+start_array[k+1]
+        //console.log(key_string)
+        if(rules.hasOwnProperty(key_string)) {
+            //console.log(rules[key_string])
+            tmp_array.push(start_array[k], rules[key_string])
+        }
+        else {
+            tmp_array.push(start_array[k])
+        }
+    }
+    start_array = tmp_array
+}
+
+start_array
