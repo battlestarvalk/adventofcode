97bf98b26ba50e6e10bd3b682d5f879a20adac86
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2021-12-15 16:48:19 +0000

    Update aoc2021_day14_extendedPolymerisation.js

--- 2021/aoc2021_day14_extendedPolymerisation.js
+++ 2021/aoc2021_day14_extendedPolymerisation.js
@@ -7,7 +7,8 @@ tmp_input = input.slice(2)
 
 for(var i=0;i<tmp_input.length;i++) {
     tmp = tmp_input[i].split(' -> ')
-    rules[tmp[0]] = [tmp[1], 0]
+    //rules[tmp[0]] = [tmp[1], 0]
+    rules[tmp[0]] = [tmp[1], i]
 }
 
 for(var j=0;j<start_string.length-1;j++) {
@@ -26,4 +27,18 @@ for(pair in rules) {
     rules[pair][1] -= tmp_val
 
 }
+
+tmp_rules = new Array(16).fill(0)
+for(pair in rules) {  
+    var tmp_val = tmp_rules[rules[pair][1]],
+        tmp_char = rules[pair][0],
+        first_pair = rules[pair.substr(0,1) + tmp_char][1],
+        second_pair = rules[tmp_char + pair.substr(1,2)][1];
+console.log(pair, rules[pair], first_pair, second_pair, tmp_val, tmp_char, tmp_rules)
+    tmp_rules[first_pair] += tmp_val
+    tmp_rules[second_pair] += tmp_val
+    tmp_rules[pair] -= tmp_val
+
+}
+tmp_rules
 
