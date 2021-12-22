2d673157129e41c704e5346816fb5c4578baaa57
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2021-12-22 19:13:51 +0000

    Update aoc2021_day22_reactorReboot_incomplete.js

--- 2021/aoc2021_day22_reactorReboot_incomplete.js
+++ 2021/aoc2021_day22_reactorReboot_incomplete.js
@@ -1,4 +1,8 @@
+//smaller ex
 input = document.querySelector('pre').textContent.split('\n').slice(0,-1)
+
+//larger ex
+input = document.querySelectorAll('pre')[1].textContent.split('\n').slice(0,-3)
 
 reboot = new Array(input.length)
 
@@ -18,117 +22,83 @@ function rangeFinder(oldArr, testArr) {
         //if lower range overlap (testArr starts below oldArr and ends inside)
         if(a[0] >= b[0] && a[1] >= b[1]) {
             // returns ALREADY ON selection
-            return /*[ [b[0], a[0]], */
-                     [a[0], b[1]]
-                   //]
+            return [ a[0], b[1] ];
         }
 
         //if upper range overlap (testArr starts inside oldArr and ends above)
-        else if(b[1] >= a[1] && b[0] >= a[0]) {
+        else if(b[0] >= a[0] && b[1] >= a[1] ) {
             // returns ALREADY ON selection
-            return /*[*/ [b[0], a[1]] /*, 
-                     [a[1], b[1]] 
-                   ]*/
+            return [ b[0], a[1] ];
         }
 
         // if testArr laps the whole range
         else { 
             // returns the ALREADY ON section
-          return [ 
-                    a[0], a[1]
-                 /* [ [b[0], a[0]], [a[0], b[1]] ], 
-                  [ [b[0], a[1]], [a[1], b[1]] ] */
-                  ] 
+          return [ a[0], a[1] ];
         }
     }
 
-    else {return 'none'}
+    else {return 'none';}
 
 }
 
+/*DOESN'T WORK WHEN NEGATIVES ARE INVOLVED*/
 on_cubes = 0
-x = []
-y = []
-z = []
+cubes = [[],[],[]]
+dimensions = ['X','Y','Z']
+
 for(var i = 0; i<input.length; i++) {
-    if( i == 0) {
-        x.push([reboot[i]['startX'], reboot[i]['endX']])
-        y.push([reboot[i]['startY'], reboot[i]['endY']])
-        z.push([reboot[i]['startZ'], reboot[i]['endZ']])
-console.log(reboot[i], x)
+        
+    posVal = [0,0,0]
+    negVal = [0,0,0]
+
+    if( i == 0 ) {
+        for(var k=0; k<3;k++) {
+            dim = dimensions[k]
+            cubes[k].push([reboot[i]['start'+dim], reboot[i]['end'+dim]])
+                        posVal[k] = (reboot[i]['end'+dim] - reboot[i]['start'+dim]) + 1
+        }
+        on_cubes += posVal.reduce((a,b) => a*b)
+
         continue;
     }
-    xVal = 0
-    yVal = 0
-    zVal = 0
+   
+    for(var k=0; k<3;k++) {
+        dim = dimensions[k]
+        start = reboot[i]['start'+dim]
+        end = reboot[i]['end'+dim]
+        for(var j=0; j<dim.length;j++) {
+            range = rangeFinder(cubes[k][j], [start, end])
+            if(range != 'none') {
+                break;
+            }
+         }
 
-    xValneg = 0
-    yValneg = 0
-    zValneg = 0
+        posVal[k] = (end - start) + 1
 
-    for(var j=0; j<x.length;j++) {
-        xRange = rangeFinder(x[j], [reboot[i]['startX'], reboot[i]['endX']])
-        if(xRange != 'none') {
-            break;
+        if(range == 'none') {
+            cubes[k].push([start, end]) 
         }
-     }
+        else if(range != 'none') {
+            negVal[k] = (range[1] - range[0]) + 1
+            if(reboot[i]['state'] == 'on') {
+                cubes[k][j][0] = Math.min(start, cubes[k][j][0])
+                cubes[k][j][1] = Math.max(end, cubes[k][j][1])
+               }
 
-    for(var k=0; k<y.length;k++) {
-        yRange = rangeFinder(y[k], [reboot[i]['startY'], reboot[i]['endY']])
-        if(yRange != 'none') {
-            break;
+            else if(reboot[i]['state'] == 'off') {
+                cubes[k][j][0] = Math.max(end, cubes[k][j][0])
+                cubes[k][j][1] = Math.min(start, cubes[k][j][1])
+               }
         }
-     }
-
-    for(var m=0; m<z.length;m++) {
-        zRange = rangeFinder(z[m], [reboot[i]['startZ'], reboot[i]['endZ']])
-        if(zRange != 'none') {
-            break;
-        }
-     }
-
-    xVal = (reboot[i]['endX'] - reboot[i]['startX']) + 1
-
-    if(xRange == 'none') {
-        x.push([reboot[i]['startX'], reboot[i]['endX']])  
+    
     }
-    if(xRange != 'none') {
-        xValneg = (xRange[1] - xRange[0]) + 1
-        x[j][0] = Math.min(reboot[i]['startX'], x[j][0])
-        x[j][0] = Math.max(reboot[i]['endX'], x[j][1])
+    if(reboot[i]['state'] == 'on') {
+        on_cubes += (posVal.reduce((a,b) => a*b) - negVal.reduce((a,b) => a*b))
     }
 
-    yVal = (reboot[i]['endY'] - reboot[i]['startY']) + 1
-
-    if(yRange == 'none') {
-        y.push([reboot[i]['startY'], reboot[i]['endY']])  
-    }
-    if(yRange != 'none') {
-        yValneg = (yRange[1] - yRange[0]) + 1
-        y[k][0] = Math.min(reboot[i]['startY'], y[k][0])
-        y[k][0] = Math.max(reboot[i]['endY'], y[k][1])
+    if(reboot[i]['state'] == 'off') {
+        on_cubes -= (negVal.reduce((a,b) => a*b))
     }
 
-    zVal = (reboot[i]['endZ'] - reboot[i]['startZ']) + 1
-
-    if(zRange == 'none') {
-        z.push([reboot[i]['startZ'], reboot[i]['endZ']])  
-    }
-    if(zRange != 'none') {
-        zValneg = (zRange[1] - zRange[0]) + 1
-        z[m][0] = Math.min(reboot[i]['startZ'], z[m][0])
-        z[m][0] = Math.max(reboot[i]['endZ'], z[m][1])
-    }
-
-
-console.log(reboot[i], xRange, x, xVal, yVal, zVal, xValneg, yValneg, zValneg)
 }
-
-// inner mechanisms of example
-(3*3*3)
-+
-(3*3*3 - 2*2*2)
--
-(2*2*2)
-+
-(1*1*1)
