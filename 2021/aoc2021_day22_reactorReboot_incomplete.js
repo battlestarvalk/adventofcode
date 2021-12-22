431b69e694b777466d0b54c3e9daf03dccd95bf9
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2021-12-22 19:54:17 +0000

    Update aoc2021_day22_reactorReboot_incomplete.js

--- 2021/aoc2021_day22_reactorReboot_incomplete.js
+++ 2021/aoc2021_day22_reactorReboot_incomplete.js
@@ -1,3 +1,32 @@
+//detects if range is overlapping and if so, which range
+function rangeFinder(oldArr, testArr) {
+    a = oldArr
+    b = testArr
+
+    //if overlap, return the range of lights already on
+    if(Math.max(a[0], b[0]) - Math.min(a[1], b[1]) <= 0) {
+        //if lower range overlap (testArr starts below oldArr and ends inside)
+        if(a[0] >= b[0] && a[1] >= b[1]) {
+            return [ a[0], b[1] ];
+        }
+
+        //if upper range overlap (testArr starts inside oldArr and ends above)
+        else if(b[0] >= a[0] && b[1] >= a[1] ) {
+            return [ b[0], a[1] ];
+        }
+
+        // if testArr laps the whole range
+        else { 
+          return [ a[0], a[1] ];
+        }
+    }
+
+    // else, say nothing
+    else {return 'none';}
+
+}
+
+
 //smaller ex
 input = document.querySelector('pre').textContent.split('\n').slice(0,-1)
 
@@ -10,93 +39,86 @@ for(var i=0; i<input.length;i++) {
     instr = input[i].match(/(on|off) x=(-?[0-9]+)..(-?[0-9]+),y=(-?[0-9]+)..(-?[0-9]+),z=(-?[0-9]+)..(-?[0-9]+)/)
     reboot[i] = {string: instr[0], state:instr[1], startX:Number(instr[2]), endX:Number(instr[3]), startY:Number(instr[4]), endY:Number(instr[5]), startZ:Number(instr[6]), endZ:Number(instr[7])}
 }
-reboot
 
-//detects if range is overlapping and if so, which range
-function rangeFinder(oldArr, testArr) {
-    a = oldArr
-    b = testArr
+/*DOESN'T WORK ON LONGER EXAMPLE*/
 
-    //if overlap
-    if(Math.max(a[0], b[0]) - Math.min(a[1], b[1]) <= 0) {
-        //if lower range overlap (testArr starts below oldArr and ends inside)
-        if(a[0] >= b[0] && a[1] >= b[1]) {
-            // returns ALREADY ON selection
-            return [ a[0], b[1] ];
-        }
-
-        //if upper range overlap (testArr starts inside oldArr and ends above)
-        else if(b[0] >= a[0] && b[1] >= a[1] ) {
-            // returns ALREADY ON selection
-            return [ b[0], a[1] ];
-        }
-
-        // if testArr laps the whole range
-        else { 
-            // returns the ALREADY ON section
-          return [ a[0], a[1] ];
-        }
-    }
-
-    else {return 'none';}
-
-}
-
-/*DOESN'T WORK WHEN NEGATIVES ARE INVOLVED*/
+//set up variables that shouldn't reset
+// cubes that are switched on
 on_cubes = 0
+// cube ranges that are on in each direction
 cubes = [[],[],[]]
+// dimensions (for Quick Red)
 dimensions = ['X','Y','Z']
 
-for(var i = 0; i<input.length; i++) {
-        
+// for each instruction
+for(var i = 0; i<reboot.length; i++) {
+    // should be fresh each time
+    // posVal = off cubes to be switched ON
     posVal = [0,0,0]
+    // negVal = on cubes to be left/switched OFF
     negVal = [0,0,0]
 
+    // set up the array on the first cycle
     if( i == 0 ) {
         for(var k=0; k<3;k++) {
             dim = dimensions[k]
             cubes[k].push([reboot[i]['start'+dim], reboot[i]['end'+dim]])
-                        posVal[k] = (reboot[i]['end'+dim] - reboot[i]['start'+dim]) + 1
+            posVal[k] = (reboot[i]['end'+dim] - reboot[i]['start'+dim]) + 1
         }
+        // first round is always on, no overlaps to worry about
         on_cubes += posVal.reduce((a,b) => a*b)
-
         continue;
     }
    
+    //for each dimension
     for(var k=0; k<3;k++) {
+        //dimension letter
         dim = dimensions[k]
+        //start and end of dimension
         start = reboot[i]['start'+dim]
         end = reboot[i]['end'+dim]
-        for(var j=0; j<dim.length;j++) {
+
+        //distance of lights we want switched ON
+        // using math.max and math.abs to prevent any negative number nonsense
+		posVal[k] = Math.abs(Math.max(end, start) - Math.min(start)) + 1
+
+        //we want to account for EVERY overlap
+		overlap = false
+        for(var j=0; j<cubes[k].length;j++) {
+            // feed in each lit range and push out the currently lit range
             range = rangeFinder(cubes[k][j], [start, end])
+
+            // if it finds a hit
             if(range != 'none') {
-                break;
+                //negative val from the dimension - cubes that are CURRENTLY ON
+                negVal[k] = Math.abs(Math.max(range[1], range[0]) - Math.min(range[1], range[0])) + 1
+                //update the new lit range as appropriate - if on
+                if(reboot[i]['state'] == 'on') {
+                    cubes[k][j][0] = Math.min(start, cubes[k][j][0])
+                    cubes[k][j][1] = Math.max(end, cubes[k][j][1])
+                }
+                
+                //update the new lit range as appropriate - if off
+                else if(reboot[i]['state'] == 'off') {
+                    cubes[k][j][0] = Math.max(end, cubes[k][j][0])
+                    cubes[k][j][1] = Math.min(start, cubes[k][j][1])
+                }
+                // we found a range, so no need to push this new range to the dimensions list
+				overlap = true
             }
-         }
-
-        posVal[k] = (end - start) + 1
-
-        if(range == 'none') {
+    	}
+        //got to the end of the ranges and couldn't find a result? new lit range!
+        if(overlap == false) {
             cubes[k].push([start, end]) 
         }
-        else if(range != 'none') {
-            negVal[k] = (range[1] - range[0]) + 1
-            if(reboot[i]['state'] == 'on') {
-                cubes[k][j][0] = Math.min(start, cubes[k][j][0])
-                cubes[k][j][1] = Math.max(end, cubes[k][j][1])
-               }
-
-            else if(reboot[i]['state'] == 'off') {
-                cubes[k][j][0] = Math.max(end, cubes[k][j][0])
-                cubes[k][j][1] = Math.min(start, cubes[k][j][1])
-               }
-        }
-    
     }
+    
+    //if switching a range on, then switch ON positive cubes MINUS all the currently on 
     if(reboot[i]['state'] == 'on') {
         on_cubes += (posVal.reduce((a,b) => a*b) - negVal.reduce((a,b) => a*b))
     }
 
+    //if switching a range off, then MINUS all the currently on cubes.
     if(reboot[i]['state'] == 'off') {
         on_cubes -= (negVal.reduce((a,b) => a*b))
     }
