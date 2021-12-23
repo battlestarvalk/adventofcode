366b0af36bd656f44ceb98d702b83bd080f55923
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2021-12-23 15:04:09 +0000

    Update aoc2021_day22_reactorReboot_incomplete.js

--- 2021/aoc2021_day22_reactorReboot_incomplete.js
+++ 2021/aoc2021_day22_reactorReboot_incomplete.js
@@ -48,11 +48,11 @@ for(var i=0; i<input.length;i++) {
 on_cubes = 0
 // cube ranges that are on in each direction
 cubes = [[],[],[]]
-// dimensions (for Quick Red)
+// dimensions (for Quick Read)
 dimensions = ['X','Y','Z']
 
 // for each instruction
-for(var i = 0; i<12; i++) {
+for(var i = 0; i<reboot.length; i++) {
     // should be fresh each time
     // posVal = off cubes to be switched ON
     posVal = [0,0,0]
@@ -80,39 +80,31 @@ for(var i = 0; i<12; i++) {
         start = reboot[i]['start'+dim]
         end = reboot[i]['end'+dim]
 
+console.log(i, reboot[i]['state'], dim, start, end, cubes[k].flat())
+
         //distance of lights we want switched ON
         // using math.max and math.abs to prevent any negative number nonsense
 		posVal[k] = Math.abs(Math.max(end, start) - Math.min(start)) + 1
-
-        ranges = 0
-
-        if(cubes[k].length > 1) {
-            for(var j=0; j<cubes[k].length; j++) {
-                range = rangeFinder(cubes[k][j], [start, end])
-                console.log(range)
-                ranges++
-            }
-        }
-
-        if(cubes[k].length == 1 || ranges == 1) {
+        for(var j=0; j<cubes[k].length;j++) {
+            overlap = false
             // feed in each lit range and push out the currently lit range
-            range = rangeFinder(cubes[k][0], [start, end])
-  
+            range = rangeFinder(cubes[k][j], [start, end])
+           
             // if it finds a hit
             if(range != 'none') {
                 //negative val from the dimension - cubes that are CURRENTLY ON
                 negVal[k] = Math.abs(Math.max(range[0][1], range[0][0]) - Math.min(range[0][1], range[0][0])) + 1
                 //update the new lit range as appropriate - if on
                 if(reboot[i]['state'] == 'on') {
-                    cubes[k][0][0] = Math.min(start, cubes[k][0][0])
-                    cubes[k][0][1] = Math.max(end, cubes[k][0][1])
+                    cubes[k][j][0] = Math.min(start, cubes[k][j][0])
+                    cubes[k][j][1] = Math.max(end, cubes[k][j][1])
                 }
-                
+
                 //update the new lit range as appropriate - if off
                 else if(reboot[i]['state'] == 'off') {
                     if(range[1].flat().length == 2) {
-                        cubes[k][0][0] = range[1][0]
-                        cubes[k][0][1] = range[1][1]
+                        cubes[k][j][0] = range[1][0]
+                        cubes[k][j][1] = range[1][1]
                     }
 
                     else if(range[1].flat().length > 2) {
@@ -121,19 +113,15 @@ for(var i = 0; i<12; i++) {
                     }
                 }
                 // we found a range, so no need to push this new range to the dimensions list
-				overlap = true
+                overlap = true
             }
 
-            else if(range == 'none') {
+            else if(overlap == false) {
                cubes[k].push([start, end]) 
             }
+
         }
 
-//ISSUE: need to figure out how to "rejoin" ranges - pull all overlapping ranges, slice out of original, place in lowest/highest?
-        else if(ranges > 1) {
-         console.log('multiple ranges', ranges, cubes[k], start, end)
-        break;
-        }
 
     }
 
