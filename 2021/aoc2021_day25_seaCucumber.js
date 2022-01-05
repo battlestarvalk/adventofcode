2caa73dfac7a1609cb0e0d5cdee0615186a7438b
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2022-01-05 15:35:04 +0000

    Update aoc2021_day25_seaCucumber.js

--- 2021/aoc2021_day25_seaCucumber.js
+++ 2021/aoc2021_day25_seaCucumber.js
@@ -1,48 +1,82 @@
+/*part one*/
 input = document.querySelector('pre').textContent.split('\n').slice(0,-1).map(x => x.split(''))
-tmp_array = new Array(input.length).fill('.').map(x => x = new Array(input[0].length).fill('.'))
 
-tmp_array = new Array(input.length).fill('.').map(x => x = new Array(input[0].length).fill('.'))
-for(var i=0; i<input.length;i++) {
-    for(var j=0;j<input[i].length;j++) {
-        if(input[i][j] == 'v') {
-            if(i == input.length-1) {
-                if(input[0][j] == 'v') {
-                    tmp_array[0][j] = 'v'
+safe_input = input
+
+for (var k=0;; k++) {
+
+    tmp_array = new Array(input.length).fill('.').map(x => x = new Array(input[0].length).fill('.'))
+
+    cucumberMovement = 0
+
+    for(var i=0; i<input.length;i++) {
+        for(var j=0;j<input[i].length;j++) {
+            if(safe_input[i][j] == 'v') {
+                widthCheckLeft = j-1
+                widthCheckRight = j+1
+
+                if(j == safe_input[i].length-1) {
+                    widthCheckRight = 0
+                }
+
+                if(j == 0) {
+                    widthCheckLeft = input[i].length-1
+                }
+
+                if(i == safe_input.length-1) {
+                    if((safe_input[0][j] == '.' && safe_input[0][widthCheckLeft] != '>') || 
+                        (safe_input[0][j] == '>' && safe_input[0][widthCheckRight] == '.') ) {
+                        tmp_array[0][j] = 'v'
+                        cucumberMovement++
+                    }
+
+                    else {
+                    tmp_array[i][j] = 'v'
+                    }   
+                }
+
+                else if((safe_input[i+1][j] == '.' && safe_input[i+1][widthCheckLeft] != '>') || 
+                        (safe_input[i+1][j] == '>' && safe_input[i+1][widthCheckRight] == '.') ) {
+                    tmp_array[i+1][j] = 'v'
+                    cucumberMovement++
                 }
 
                 else {
-                tmp_array[i][j] = 'v'
-                }   
+                    tmp_array[i][j] = 'v'
+                }  
+
             }
 
-            else if(input[i+1][j] == '.') {
-                tmp_array[i+1][j] = 'v'
-            }
+          if(safe_input[i][j] == '>') {
+                if(j == safe_input[i].length-1) {
+                    if(safe_input[i][0] == '.') {
+                        tmp_array[i][0] = '>'
+                        cucumberMovement++
+                    }
 
-            else {
-                tmp_array[i][j] = 'v'
-            }   
-        }
+                    else {
+                    tmp_array[i][j] = '>'
+                    }   
+                }
 
-      if(input[i][j] == '>') {
-            if(i == input[i].length-1) {
-                if(input[i][0] == '>') {
-                    tmp_array[i][0] = '>'
+                else if(safe_input[i][j+1] == '.') {
+                    tmp_array[i][j+1] = '>'
+                    cucumberMovement++
                 }
 
                 else {
-                tmp_array[i][j] = '>'
+                    tmp_array[i][j] = '>'
                 }   
             }
-
-            else if(input[i][j+1] == '.') {
-                tmp_array[i][j+1] = '>'
-            }
-
-            else {
-                tmp_array[i][j] = '>'
-            }   
         }
     }
+
+    safe_input = tmp_array
+
+   if(cucumberMovement == 0) {
+        console.log(k)
+        break;
+    }
+
 }
-tmp_array
+
