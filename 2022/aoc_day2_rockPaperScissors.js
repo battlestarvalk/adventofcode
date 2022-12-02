9672c34a374ca1c90467b1a9bf8acc17d1a414fd
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2022-12-02 10:27:07 +0000

    Update aoc_day2_rockPaperScissors.js

--- 2022/aoc_day2_rockPaperScissors.js
+++ 2022/aoc_day2_rockPaperScissors.js
@@ -82,3 +82,92 @@ for(var i=0; i<input.length; i++) {
 return scores.reduce(function(a,b) {return a+b})
   
 }
+
+strategyGuide(input)
+
+
+/*partTwo*/
+
+function losingStrat(input) {
+scores = []
+for(var i=0; i<input.length; i++) {
+  input[i].charAt(0)
+  input[i].charAt(2)
+
+  playScore = 0
+  resultScore = 0
+
+  result = ""
+  if(input[i].charAt(0) == "A") {
+      switch(input[i].charAt(2)) {
+          case "X":
+              playScore = 3;
+              break;
+          case "Y":
+              playScore = 1;
+              break;
+          case "Z":
+              playScore = 2;
+              break;
+      }
+  }
+
+  if(input[i].charAt(0) == "B") {
+      switch(input[i].charAt(2)) {
+          case "X":
+              playScore = 1;
+              break;
+          case "Y":
+              playScore = 2;
+              break;
+          case "Z":
+              playScore = 3;
+              break;
+      }
+  }
+
+  if(input[i].charAt(0) == "C") {
+      switch(input[i].charAt(2)) {
+          case "X":
+              playScore = 2;
+              break;
+          case "Y":
+              playScore = 3;
+              break;
+          case "Z":
+              playScore = 1;
+              break;
+      }
+  }
+
+  switch(input[i].charAt(2)) {
+      case "X":
+          result = "loss";
+          break;
+      case "Y":
+          result = "draw";
+          break;
+      case "Z":
+          result = "win";
+          break;
+  }
+
+  switch(result) {
+      case "win":
+          resultScore = 6;
+          break;
+      case "loss":
+          resultScore = 0;
+          break;
+      case "draw":
+          resultScore = 3;
+          break;
+  }
+  scores.push(playScore + resultScore)
+}
+
+return scores.reduce(function(a,b) {return a+b})
+  
+}
+
+losingStrat(input)
