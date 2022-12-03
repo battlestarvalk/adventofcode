f9729d710311d33b7dc7ab2ef91c61893f47ec6c
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2022-12-03 12:14:00 +0000

    Update aoc_day3_rucksackReorganisation.js

--- 2022/aoc_day3_rucksackReorganisation.js
+++ 2022/aoc_day3_rucksackReorganisation.js
@@ -1,7 +1,9 @@
 var input = document.querySelector('pre').textContent.split('\n').slice(0,-1);
+const isUpperCase = (string) => /^[A-Z]*$/.test(string)
 
 /*part one*/
 
+/*
 /*split out the two compartments*/
 rucksackContents = []
 for(var i=0; i<input.length;i++) {
@@ -15,7 +17,7 @@ for(var i = 0; i<rucksackContents.length; i++) {
 
 rucksackShared.push(    rucksackContents[i][0].filter(element => rucksackContents[i][1].includes(element))[0] )
 
-} 
+}
 
 /* assign value */
 const isUpperCase = (string) => /^[A-Z]*$/.test(string)
@@ -33,11 +35,29 @@ else {
   
 }
 
+*/
+
+/*OPTIMISED*/
+contentValue = []
+for(var i=0; i<input.length;i++) {
+    splitLoc = (input[i].length / 2)
+    compartments = ([input[i].slice(0, splitLoc).split(""), input[i].slice(splitLoc).split("")])
+
+    sharedVal = compartments[0].filter(element => compartments[1].includes(element))[0]
+
+        if(isUpperCase(sharedVal)) {
+        contentValue.push(sharedVal.toLowerCase().charCodeAt(0) - 70)
+    }
+        else {
+        contentValue.push(sharedVal.toLowerCase().charCodeAt(0) - 96)
+    }
+  
+}
+
 /*sum backpack*/
 contentValue.reduce(function(a,b) {return a+b})
 
 /*part two*/
-var input = document.querySelector('pre').textContent.split('\n').slice(0,-1);
 
 elfCarry = [[]] 
 
