8f2e1b4689bef43164b1f99dbf276ab1f0830478
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2022-12-04 21:32:46 +0000

    Update aoc_day4_campCleanup.js

--- 2022/aoc_day4_campCleanup.js
+++ 2022/aoc_day4_campCleanup.js
@@ -1,6 +1,6 @@
 var input = document.querySelector('pre').textContent.split('\n').slice(0,-1); 
 
-/*part one*/
+/*prep for both*/
 elfAssignments = new Array(input.length)
 
 for(var i=0; i<input.length;i++) {
@@ -8,6 +8,7 @@ for(var i=0; i<input.length;i++) {
     elfAssignments[i] = {string: instr[0], first:[Number(instr[1]), Number(instr[2])], second:[Number(instr[3]), Number(instr[4])]}
 }
 
+/*part one*/
 function totalOverlapFinder(oldArr, testArr) {
     a = oldArr
     b = testArr
@@ -30,4 +31,27 @@ for(var i=0; i<input.length;i++) {
 
 totalOverlaps
 
-/* part two */
+/*part two*/
+function anyOverlapFinder(oldArr, testArr) {
+    a = oldArr
+    b = testArr
+
+    //if overlap, return the range of lights already on
+    if(Math.max(a[0], b[0]) - Math.min(a[1], b[1]) <= 0) {
+    	return "overlap"
+    }
+
+    // else, say nothing
+    else {return 'none';}
+
+}
+
+anyOverlaps = 0
+for(var i=0; i<input.length;i++) {
+    x = elfAssignments[i]
+    if(anyOverlapFinder(x["first"], x["second"]) != "none") {
+        anyOverlaps++
+    }
+}
+
+anyOverlaps
