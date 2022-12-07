5b8b8f34b5ea0ae24dfef4143e62ca29ffdb9271
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2022-12-07 09:09:19 +0000

    Create aoc_day7_noSpace.js

---
+++ 2022/aoc_day7_noSpace.js
@@ -0,0 +1,53 @@
+var input = document.querySelectorAll('pre')[1].textContent.split('\n').slice(0,-1); 
+
+/* builds directory */
+directory = ""
+
+drive = {}
+
+for(var i=0; i<input.length; i++) {
+    if(/^\$ cd/.test(input[i])) {
+        dir = input[i].match(/^\$ cd (.+)/)[1]
+        if(dir != "..") {
+            if(!path[dir]) {
+                drive[dir] = {path: directory, file:[], size: 0}
+            }
+
+            drive[dir]["path"] += dir
+            directory = drive[dir]["path"]
+        }
+
+        if(dir == "..") {
+            directory = directory.slice(0, -1)
+        }
+    }
+
+    if(/^\$ ls/.test(input[i])) {
+        for(var j=i+1; j<input.length; j++) {
+            if(/^\$ cd/.test(input[j])) {
+                i = j-1
+                break;
+            }
+
+            else {
+                if(/^dir .+/.test(input[j])) {
+                    sub_dir = input[j].match(/^dir (.+)/)[1]
+                    if(!drive[sub_dir]) {
+                        drive[sub_dir] = {path: directory, file:[], size: 0}
+                    }
+                }
+                 else {
+                     cd = directory[directory.length-1]
+                     vals = input[j].match(/([0-9]+) ([a-z.]+)/)
+                     drive[cd]["file"].push([vals[2], vals[1]])
+                     
+                     for(var k=1; k<directory.length+1; k++) {
+                         cd = directory[directory.length-k]
+                         drive[cd]["size"] += Number(vals[1])
+                     }
+                 }
+            }
+
+        }  
+    }
+}
