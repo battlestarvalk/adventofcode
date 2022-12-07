e22e41974d7ee0f7df6bf92f6c5cb390f84aa74f
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2022-12-07 09:56:04 +0000

    Update aoc_day7_noSpace.js

--- 2022/aoc_day7_noSpace.js
+++ 2022/aoc_day7_noSpace.js
@@ -1,25 +1,29 @@
 var input = document.querySelectorAll('pre')[1].textContent.split('\n').slice(0,-1); 
 
 /* builds directory */
-directory = ""
+directory = []
+//directory = ""
 
 drive = {}
 
 for(var i=0; i<input.length; i++) {
-    if(/^\$ cd/.test(input[i])) {
+// has issue - saves the whole array to the directory??
+    if(/^\$ cd /.test(input[i])) {
         dir = input[i].match(/^\$ cd (.+)/)[1]
         if(dir != "..") {
-            if(!path[dir]) {
+            if(!drive[dir]) {
                 drive[dir] = {path: directory, file:[], size: 0}
             }
-
-            drive[dir]["path"] += dir
+            //drive[dir]["path"] += dir
+            drive[dir]["path"].push(dir) 
             directory = drive[dir]["path"]
+
         }
 
         if(dir == "..") {
             directory = directory.slice(0, -1)
         }
+
     }
 
     if(/^\$ ls/.test(input[i])) {
@@ -30,7 +34,7 @@ for(var i=0; i<input.length; i++) {
             }
 
             else {
-                if(/^dir .+/.test(input[j])) {
+                if(/^dir/.test(input[j])) {
                     sub_dir = input[j].match(/^dir (.+)/)[1]
                     if(!drive[sub_dir]) {
                         drive[sub_dir] = {path: directory, file:[], size: 0}
@@ -50,4 +54,15 @@ for(var i=0; i<input.length; i++) {
 
         }  
     }
+
 }
+
+drive
+
+small_dirs = []
+for(const dir in drive) {
+    if(drive[dir]["size"] <= 100000) {
+        small_dirs.push(drive[dir]["size"]) 
+    }
+}
+small_dirs.reduce(function(a,b) {return a+b})
