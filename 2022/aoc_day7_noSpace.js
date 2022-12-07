d9ab3ccec65ea512883f9fe87d9cd79a917774f0
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2022-12-07 12:58:01 +0000

    Update aoc_day7_noSpace.js

--- 2022/aoc_day7_noSpace.js
+++ 2022/aoc_day7_noSpace.js
@@ -9,12 +9,16 @@ for(var i=0; i<input.length; i++) {
     if(/^\$ cd /.test(input[i])) {
         dir = input[i].match(/^\$ cd (.+)/)[1]
         if(dir != "..") {
+              
             if(!(dir in drive)) {
-                drive[dir] = {path: directory.slice(), file:[], size: 0}
+                drive[dir] = {path: directory.slice(), file_names:[], file_sizes:[], size: 0}
             }
 
-            drive[dir]["path"].push(dir) 
-            directory = drive[dir]["path"]
+            if(dir != directory[directory.length-1]) {
+                drive[dir]["path"].push(dir) 
+                directory = drive[dir]["path"]
+            }
+  
         }
 
         if(dir == "..") {
@@ -34,20 +38,22 @@ for(var i=0; i<input.length; i++) {
                 if(/^dir/.test(input[j])) {
                     sub_dir = input[j].match(/^dir (.+)/)[1]
                     if(!(sub_dir in drive)) {
-                        drive[sub_dir] = {path: directory.slice(), file:[], size: 0}
+                        drive[sub_dir] = {path: directory.slice(), file_names:[], file_sizes:[], size: 0}
                     }
                 }
                  else {
                      cd = directory[directory.length-1]
                      vals = input[j].match(/([0-9]+) ([a-z.]+)/)
 
-                     if(!(drive[cd]["file"].includes([vals[2], vals[1]]))) {
-                         drive[cd]["file"].push([vals[2], vals[1]])
-                     }
+                     if(!(drive[cd]["file_names"].includes(vals[2]))) {
+                         drive[cd]["file_names"].push(vals[2])
+                         drive[cd]["file_sizes"].push(vals[1])
+                     
 
                      for(var k=1; k<directory.length+1; k++) {
                          cd = directory[directory.length-k]
                          drive[cd]["size"] += Number(vals[1])
+                     }
                      }
                  }
             }
