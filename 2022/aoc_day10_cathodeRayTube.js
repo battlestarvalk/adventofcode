7d6e3f943f7d4d27a009414549012031632d242d
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2022-12-11 23:05:15 +0000

    Update aoc_day10_cathodeRayTube.js

--- 2022/aoc_day10_cathodeRayTube.js
+++ 2022/aoc_day10_cathodeRayTube.js
@@ -18,3 +18,28 @@ function signals(arr, signalStrength) {
 } 
 
 [signals(input,20), signals(input,60), signals(input,100), signals(input,140), signals(input,180), signals(input,220)].reduce((a,b) => (a+b)) 
+
+CRT = new Array(6).fill('.').map(x => x = new Array(39).fill('.'))
+function signals(arr, signalStrength, row_num) {
+  registerX = 1
+  cycles = signalStrength
+
+  for (var i = 0; i<cycles-2; i++) {
+
+    if(arr[i][1] == "addx") {
+      registerX += Number(arr[i][2])
+      cycles--
+    }
+
+        if(registerX == i || registerX == i-1 || registerX == i+1) {
+                CRT[row_num][registerX] = "#"
+        }
+  }
+
+  return registerX * signalStrength
+
+} 
+
+signals(input, 20, 0)
+CRT
+CRT[0].join() 
