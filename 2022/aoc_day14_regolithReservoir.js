449e43b03373e230bd3453d8da8f3974c4d29de1
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2022-12-14 14:08:23 +0000

    Update aoc_day14_regolithReservoir.js

--- 2022/aoc_day14_regolithReservoir.js
+++ 2022/aoc_day14_regolithReservoir.js
@@ -11,3 +11,31 @@ rock_bed = Math.max(...rock_y)
 rock_width = rock_end - rock_start
 
 cave_system = new Array(rock_bed+1).fill('.').map(x => x = new Array(rock_width+1).fill('.'))
+
+for(var i=0;i<input.length;i++) {
+    for(var j=1; j<input[i].length;j++) {
+        first_co = [input[i][j-1][0] - rock_start, input[i][j-1][1]]
+        sec_co = [input[i][j][0] - rock_start, input[i][j][1]]
+
+        x_start = Math.min(...[first_co[0], sec_co[0]])
+        x_end = Math.max(...[first_co[0], sec_co[0]])
+        y_start = Math.min(...[first_co[1], sec_co[1]])
+        y_end = Math.max(...[first_co[1], sec_co[1]])
+
+        if(x_end - x_start != 0) {
+            for(var x = x_start; x <= x_end; x++) {
+                cave_system[y_start][x] = "#"
+            }
+        }
+
+        if(y_end - y_start != 0) {
+            for(var y = y_start; y <= y_end; y++) {
+                cave_system[y][x_start] = "#"
+            }
+        }
+        
+        
+    }
+}
+
+cave_system
