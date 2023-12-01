4082615c82ccc89d3698e5b52b778e8fe7834f41
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2023-12-01 08:59:51 +0000

    Update aoc_day1_trebuchet.js

--- 2023/aoc_day1_trebuchet.js
+++ 2023/aoc_day1_trebuchet.js
@@ -1,1 +1,10 @@
 var input = document.querySelectorAll('pre')[0].textContent.split('\n').slice(0,-1)
+
+calibration = []
+for(var i=0; i<input.length; i++) {
+    temp_array = [...input[i].matchAll(/[0-9]/g)]
+    value = Number(temp_array[0][0]+temp_array[temp_array.length-1][0])
+    calibration.push(value)
+}
+
+calibration.reduce(function (a, b) {return a + b;}, 0)
