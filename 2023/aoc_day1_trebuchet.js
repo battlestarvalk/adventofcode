8f69ce795ea30732a878593f24b4a664fa00a8a8
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2023-12-01 09:28:53 +0000

    Update aoc_day1_trebuchet.js

--- 2023/aoc_day1_trebuchet.js
+++ 2023/aoc_day1_trebuchet.js
@@ -8,3 +8,30 @@ for(var i=0; i<input.length; i++) {
 }
 
 calibration.reduce(function (a, b) {return a + b;}, 0)
+
+var re = /[0-9]|one|two|three|four|five|six|seven|eight|nine/g;
+var matches = [];
+for(var i=0; i<input.length;i++) {
+    var str = input[i],
+        r = [],
+        m;
+    while (m = re.exec(str)) {
+        re.lastIndex -= m[0].length - 1;
+        r.push(m[0]);
+    }
+    matches.push(r[0]+r[r.length-1])
+}
+
+for(var i=0; i<matches.length; i++) {
+    matches[i] = matches[i].replaceAll('one', 1)
+    matches[i] = matches[i].replaceAll('two', 2)
+    matches[i] = matches[i].replaceAll('three', 3)
+    matches[i] = matches[i].replaceAll('four', 4)
+    matches[i] = matches[i].replaceAll('five', 5)
+    matches[i] = matches[i].replaceAll('six', 6)
+    matches[i] = matches[i].replaceAll('seven', 7)
+    matches[i] = matches[i].replaceAll('eight', 8)
+    matches[i] = matches[i].replaceAll('nine', 9)
+}
+
+matches.map(Number).reduce(function (a, b) {return a + b;}, 0)
