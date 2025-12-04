8607705b1b18f74ee14f3b243b45770c921658f0
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2025-12-04 23:12:57 +0000

    Update aoc_day1_secretEntrance.py

--- 2025/aoc_day1_secretEntrance.py
+++ 2025/aoc_day1_secretEntrance.py
@@ -24,3 +24,29 @@ for x in input:
 
 zero_rank
 
+# part two - answer too high
+
+dial = 50
+zero_rank = 0
+
+for x in input:
+  direction = re.search('L|R', x).group()
+  distance = int(re.search('[0-9]+', x).group())
+
+  if direction == "L":
+    dial = dial - (distance%100)
+  elif direction == "R":
+    dial = dial + (distance%100)
+  else:
+    print('dunno!')
+  
+  zero_rank = zero_rank + (distance//100)
+  
+  if dial < 0:
+    dial = 100 + dial;
+    zero_rank = zero_rank + 1
+  elif dial > 99:
+    dial = dial - 100
+    zero_rank = zero_rank + 1
+
+zero_rank
