c02e13889dbfd2a4491933020b9cac272e52c042
lucy valk quill <75495217+battlestarvalk@users.noreply.github.com>
2025-12-04 22:57:02 +0000

    Create aoc_day1_secretEntrance.py

---
+++ 2025/aoc_day1_secretEntrance.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import re
+
+# part one
+dial = 50
+zero_rank = 0
+
+for x in input:
+  direction = re.search('L|R', x).group()
+  distance = int(re.search('[0-9]+', x).group())
+
+  if direction == "L":
+    dial = dial - (distance%100)
+  elif direction == "R":
+    dial = dial + (distance%100)
+
+  if dial < 0:
+    dial = 100 + dial;
+  elif dial > 99:
+    dial = dial - 100
+
+  if dial == 0:
+    zero_rank = zero_rank + 1
+
+zero_rank
+
